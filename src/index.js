9123ce034247cf118d5b62f74ceb80a78faffb8b
Luca Bruzzone <spam@unsigned.it>
2019-04-29 14:02:23 +0200

    changed import system

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import * as PIXI from 'pixi.js'
+import { Application } from 'pixi.js'
 import Player from './objects/player'
 import EnemyManager from './objects/enemyManager'
 import StageManager from './objects/stageManager'
 
-const app = new PIXI.Application(512, 600, {backgroundColor : 0x000000});
+const app = new Application(512, 600, {backgroundColor : 0x000000});
 document.body.appendChild(app.view);
 
 const player = new Player(app)
